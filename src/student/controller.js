62ccb85a489036b4c4aa507127488cc0585d1dc8
Steven Suddreth <stevensuddreth@gmail.com>
2024-02-16 17:22:52 -0500

    initial commit

---
+++ src/student/controller.js
@@ -0,0 +1,23 @@
+const pool = require('../../db');
+const queries = require('./queries');
+
+const getStudents = (req, res) => {
+    pool.query(queries.getStudents, (error, results) => {
+        if (error) throw error;
+        res.status(200).json(results.rows);
+    });
+};
+
+const getStudentById = (req, res) => {
+    const id = parseInt(req.params.id);
+    pool.query(queries.getStudentById, [id], (error, results) => {
+        if (error) throw error;
+        res.status(200).json(results.rows);
+    });
+};
+
+
+module.exports = {
+    getStudents,
+    getStudentById,
+};
