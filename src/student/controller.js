63e08059e064e1fa3efcdd7fe85021b645000d35
Steven Suddreth <stevensuddreth@gmail.com>
2024-02-17 13:36:29 -0500

    complete example API

--- src/student/controller.js
+++ src/student/controller.js
@@ -16,8 +16,85 @@ const getStudentById = (req, res) => {
     });
 };
 
+const addStudent = (req, res) => {
+    const { name, email, age, dob } = req.body;
+    pool.query(queries.checkEmailExists, [email], (error, results) => {
+        if (results.rows.length) {
+            res.send('Email already exists.');
+        }
+        pool.query(queries.addStudent, [name, email, age, dob], (error, results) => {
+            if (error) throw error;
+            res.status(201).send('Student created successfully!');
+        })
+    });
+};
+
+const removeStudent = (req, res) => {
+    const id = parseInt(req.params.id);
+    pool.query(queries.getStudentById, [id], (error, results) => {
+        const noStudentFound = !results.rows.length;
+        if (noStudentFound) {
+            res.send('Student not found.');
+        }
+        pool.query(queries.removeStudent, [id], (error, results) => {
+            if (error) throw error;
+            res.status(200).send('Student removed successfully.');
+        })
+    })
+};
+
+const updateStudentName = (req, res) => {
+    const id = parseInt(req.params.id);
+    const { name } = req.body;
+    pool.query(queries.getStudentById, [id], (error, results) => {
+        const noStudentFound = !results.rows.length;
+        if (noStudentFound) {
+            res.send('Student not found.');
+        }
+        pool.query(queries.updateStudentName, [name, id], (error, results) => {
+            if (error) throw error;
+            res.status(200).send('Student name updated successfully.');
+        });
+    })
+};
+
+const updateStudentEmail = (req, res) => {
+    const id = parseInt(req.params.id);
+    const { email } = req.body;
+    pool.query(queries.getStudentById, [id], (error, results) => {
+        const noStudentFound = !results.rows.length;
+        if (noStudentFound) {
+            res.send('Student not found.');
+        }
+        pool.query(queries.updateStudentEmail, [email, id], (error, results) => {
+            if (error) throw error;
+            res.status(200).send('Student email updated successfully.');
+        });
+    })
+};
+
+const updateStudentAge = (req, res) => {
+    const id = parseInt(req.params.id);
+    const { age } = req.body;
+    pool.query(queries.getStudentById, [id], (error, results) => {
+        const noStudentFound = !results.rows.length;
+        if (noStudentFound) {
+            res.send('Student not found.');
+        }
+        pool.query(queries.updateStudentAge, [age, id], (error, results) => {
+            if (error) throw error;
+            res.status(200).send('Student age updated successfully.');
+        });
+    })
+};
+
 
 module.exports = {
     getStudents,
     getStudentById,
+    addStudent,
+    removeStudent,
+    updateStudentName,
+    updateStudentEmail,
+    updateStudentAge,
 };
--- src/student/routes.js
+++ src/student/routes.js
@@ -5,7 +5,12 @@ const controller = require('./controller');
 const router = Router();
 
 router.get('/', controller.getStudents);
+router.post('/', controller.addStudent);
 router.get('/:id', controller.getStudentById);
+router.put('/:id', controller.updateStudentName);
+router.put('/email/:id', controller.updateStudentEmail);
+router.put('/age/:id', controller.updateStudentAge);
+router.delete('/:id', controller.removeStudent);
 
 
 module.exports = router;
